374a4ea0f609bc63edcc5a8d8ee1cc405782c5fa
Michelle Guan <michelle.guan93@gmail.com>
2021-05-29 14:33:19 -0700

    Implment factorial, sum, and sumArray functions

--- src/recursion.js
+++ src/recursion.js
@@ -7,16 +7,41 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+  if (n < 0) {
+    return null;
+  }
+  if (n === 0) {
+    return 1;
+  }
+  return (n * factorial(n - 1));
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  var result = 0;
+  if (!Array.isArray(array)){
+    return array;
+  } else {
+    array.forEach(function(element){
+      result += sum(element);
+    })
+  }
+  return result;
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  var result = 0;
+  if (!Array.isArray(array)){
+    return array;
+  } else {
+    array.forEach(function(element){
+      result += arraySum(element);
+    })
+  }
+  return result;
 };
 
 // 4. Check if a number is even.
